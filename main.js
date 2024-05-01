1b6dc2374cb4798fa48accfdb382c611a83e8d3a
federicoalagna <federicoalagna01@gmail.com>
2024-05-01 19:23:58 +0200

    ciclo fizzbuzz completo

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 'use strict'
+
+// ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0) {
+
+    console.log("FizzBuzz");
+        // multiplo di 3 e 5 
+    } else if (i % 3 === 0) {
+        console.log("Fizz"); 
+        // multiplo di 3 
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+        // multiplo di 5 
+    } else {
+        console.log(i);
+        // non multiplo di 3 o 5
+    }
+
+}
